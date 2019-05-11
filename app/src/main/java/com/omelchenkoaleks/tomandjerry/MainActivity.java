33dcd4f6592e01b70b98dc94220f6b5b0326e056
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-05-11 07:19:55 +0300

    anim 2

--- app/src/main/java/com/omelchenkoaleks/tomandjerry/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/tomandjerry/MainActivity.java
@@ -10,33 +10,54 @@ import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageSwitcher mImageSwitcher;
+//    ImageSwitcher mImageSwitcher;
+
+    boolean isTomVisible = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mImageSwitcher = findViewById(R.id.image_switcher);
-        Animation inAnimation = new AlphaAnimation(0, 1);
-        inAnimation.setDuration(3000);
-        Animation outAnimation = new AlphaAnimation(1, 0);
-        outAnimation.setDuration(3000);
 
-        mImageSwitcher.setInAnimation(inAnimation);
-        mImageSwitcher.setOutAnimation(outAnimation);
-    }
-
-    public void onSwitcherClick(View view) {
-        mImageSwitcher.showNext();
-    }
-
-//    public void eraseTom(View view) {
-//        ImageView tomImageView = findViewById(R.id.tom_image_view);
-//        ImageView jerryImageView = findViewById(R.id.jerry_image_view);
+        /*
+            1-й вариант переключения анимации картинки
+         */
+//        mImageSwitcher = findViewById(R.id.image_switcher);
+//        Animation inAnimation = new AlphaAnimation(0, 1);
+//        inAnimation.setDuration(3000);
+//        Animation outAnimation = new AlphaAnimation(1, 0);
+//        outAnimation.setDuration(3000);
 //
-//        tomImageView.animate().alpha(0).setDuration(3000);
-//        jerryImageView.animate().alpha(1).setDuration(3000);
+//        mImageSwitcher.setInAnimation(inAnimation);
+//        mImageSwitcher.setOutAnimation(outAnimation);
+    }
+
+//    public void onSwitcherClick(View view) {
+//        mImageSwitcher.showNext();
 //    }
+
+    public void eraseTom(View view) {
+
+        if (isTomVisible) {
+            ImageView tomImageView = findViewById(R.id.tom_image_view);
+            tomImageView.animate().alpha(0).setDuration(3000);
+
+            ImageView jerryImageView = findViewById(R.id.jerry_image_view);
+            jerryImageView.animate().alpha(1).setDuration(3000);
+
+            isTomVisible = false;
+
+        } else {
+
+            ImageView tomImageView = findViewById(R.id.tom_image_view);
+            tomImageView.animate().alpha(1).setDuration(3000);
+
+            ImageView jerryImageView = findViewById(R.id.jerry_image_view);
+            jerryImageView.animate().alpha(0).setDuration(3000);
+
+            isTomVisible = true;
+        }
+    }
 
 }
